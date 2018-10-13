b6468e7469ce4fdcd764ce71d1ff0fe62f6c0e7d
f74981743 <bewitchsky@gmail.com>
2018-10-13 22:29:55 +0800

    Simulate click start button for mocking timer for testing

--- src/__test__/integration.test.js
+++ src/__test__/integration.test.js
@@ -9,10 +9,19 @@ beforeEach(() => {
   wrapped = mount(<App />);
 });
 
-it('show the timer', () => {
-  jest.useFakeTimers();
-  expect(wrapped.find('.control-panel__start-btn').length).toEqual(1);
-  wrapped.find('.control-panel__start-btn').simulate('click');
-  expect(setInterval).toHaveBeenCalledTimes(1);
-  expect(setInterval).toHaveBeenLastCalledWith(expect.any(Function), 1000);
+describe('Integration test', () => {
+  it('click start button', () => {
+    jest.useFakeTimers();
+    wrapped.find('.control-panel__start-btn').simulate('click');
+    expect(setInterval).toHaveBeenCalledTimes(1);
+    expect(setInterval).toHaveBeenLastCalledWith(expect.any(Function), 1000);
+
+    // The initial remaining time is 25 minutes,
+    // run 5 minutes and expect that remaining time is 20 minutes
+    jest.runTimersToTime(60 * 1000 * 5);
+    expect(wrapped.state().remainingTime).toEqual(1200);
+
+    jest.runTimersToTime(60 * 1000 * 25);
+    expect(wrapped.state().target).toEqual(1);
+  });
 });
